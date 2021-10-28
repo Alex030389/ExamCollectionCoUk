39ead23e0f4bd89cc2ce8f7c56971d9eea5c90c7
AlexKa <x182838@gmail.com>
2021-10-28 17:21:46 +0300

    page 404

--- _src/design/script/block/__scroll-to-anchor.js
+++
@@ -1,7 +0,0 @@
-/////////////////////////////////////////// scroll to anchor
-$('a[href^="#"], *[data-href^="#"]').on('click', function(e){
-  e.preventDefault();
-  var t = 300;
-  var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-  $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
-});
--- _src/design/script/main.js
+++ _src/design/script/main.js
@@ -1,30 +1,11 @@
 'use strict';
 
-// polifill
-// @include('./block/__polyfill.js')
+//////////////////////////////////////// polifill
+@@include('./block/__polyfill.js')
 
-//========================================================
 //////////////////////////////////////// plugin initialize
-//========================================================
 svg4everybody();
 
-// objectFitImages();
-
-// lazyLoad
-// const lazyLoadInstance = new LazyLoad({
-// 	elements_selector: ".lazy"
-// });
-
-// selectric
-// $('[data-selectric]').selectric();
-
-// @include('./block/__menu.js')
+//////////////////////////////////////// plugin initialize
 @@include('./block/__slider.js')
-// @include('./block/__tab.js')
-// @include('./block/__accordion.js')
-// @include('./block/__copy-code-discount.js')
-// @include('./block/__checkboxes.js')
-// @include('./block/__btn-blur.js')
-// @include('./block/__scroll-to-anchor.js')
 @@include('./block/__scroll-to-top.js')
-// @include('./block/__stick-footer.js')
